f6831d8b792fdfdc7ccd643c47a520c73492c2ba
ManNghi <22652921.nghi@student.iuh.edu.vn>
2025-03-06 21:02:17 +0700

    Update

--- react-tuan4/src/App.jsx
+++ react-tuan4/src/App.jsx
@@ -1,31 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import NewItem from './components/NewItem'
 
 function App() {
-  const [count, setCount] = useState(0)
+  var url = 'https://67c84bd20acf98d07085e3f3.mockapi.io/recipes'
+  var [recipes, setRecipes] = useState([])
+
+  useEffect(() => {
+    var fn = fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        setRecipes(data);
+        console.log(data);
+      })
+  }, [])
 
   return (
     <>
-      <div>
-        <p>Home Your Recipe</p>
-      </div>
-      <div>
-        <h1>Emma Gonzalez's Recipe Box</h1>
-      </div>
-      <div>
+      <div class="container">
         <div>
-          <img src="" alt="" />
+          <p><span>Home ></span> <span>Your Recipe Box</span></p>
         </div>
+
         <div>
-          <div>
-            Emma Gobnzalez is a deputy editor at Cheftify. bringing her expertise as a former cooking editor at The Los Angeles Times. She is also an ...
+          <h1>Emma Gonzalez's Recipe Box</h1>
+        </div>
+
+        <div class="profile"> 
+          <div class="avatar">
+            <img src="https://res.cloudinary.com/dne3wt0ys/image/upload/v1741180745/avatar_uqbkd6.png" alt="" />
+          </div>
+          <div class="info">
+            <div class="sub-info1">
+              Emma Gobnzalez is a deputy editor at Cheftify, bringing her expertise as a former cooking editor at The Los Angeles Times.
+              She is also an accomplished author, contributing to numerous cookbooks and food publication.
+              Originally from East Los Angeles, Emma now resides in New York City, where she explore a wide range of culinary delights.
+            </div>
+            <div class="sub-info2">
+              <span>6.5k Subscribes</span>
+              <button>Share</button>
+            </div>
           </div>
         </div>
       </div>
+      <div>
+        <ul class="recipes">
+          {
+            recipes.map((recipe, index) => {
+              return (
+                <NewItem key={index} image={recipe.image} name={recipe.name} time={recipe.time} />
+              )
+            })
+          }
+        </ul>
+      </div>
+      
     </>
   )
 }
-
 export default App
--- react-tuan4/src/components/NewItem.jsx
+++ react-tuan4/src/components/NewItem.jsx
@@ -1,9 +1,17 @@
+import './NewItem.css'
 export default function NewItem({image, name, time}){
+
     return(
-        <div>
-            <img height='100px' width='100px' src={image} alt="" />
-            <p>{name}</p>
-            <p>{time}</p>
+        <div class="item">
+            <img class="image" src={image} alt="" />
+            <div class="content">
+                <div class="info">
+                    <p>{name}</p>
+                    <div class="time">{time}</div>
+                </div>
+                <img src="https://res.cloudinary.com/dne3wt0ys/image/upload/v1741261402/Icon_Button_73_dax8u2.png" alt="" />
+            </div>
+            
         </div>
     )
 }
